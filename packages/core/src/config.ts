dd97ea72f2eaf09f2eda97431ca9188d94f06d5d
Aleksandr-Karikov <karikov.aleksandr2803@gmail.com>
2025-05-20 18:03:37 +0700

    Remove getLLMConfig from config
    Removed getLLMConfig function from config.ts

--- packages/core/src/config.ts
+++ packages/core/src/config.ts
@@ -4,12 +4,3 @@ export interface LLMConfig {
   model: string;
   language: "ru" | "en";
 }
-
-export function getLLMConfig(): LLMConfig {
-  return {
-    endpoint: "",
-    apiKey: "",
-    model: "",
-    language: "en",
-  };
-}

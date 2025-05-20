22c0a2d5c4b5c1b0df87eb5483ff780ab402e6be
Aleksandr-Karikov <karikov.aleksandr2803@gmail.com>
2025-05-20 18:03:37 +0700

    Update getProposedCommits
    Updated getProposedCommits function to accept LLMConfig

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,46 +1,29 @@
+import { LLMConfig } from "./config.js";
 import {
   getGitDiff,
   getUntrackedFiles,
   gitAddFiles,
   createCommit,
 } from "./git.js";
+import { generateDiffForNewFiles } from "./lib.js";
 import { analyzeGitDiff, Commit } from "./llm.js";
 
-import fs from "fs";
-import path from "path";
+export { type LLMConfig } from "./config.js";
 
-async function generateDiffForNewFiles(files: string[]): Promise<string> {
-  let result = "";
-  for (const file of files) {
-    const content = await fs.promises.readFile(path.resolve(file), "utf-8");
-    result += `\ndiff --git a/${file} b/${file}\n`;
-    result += `new file mode 100644\n`;
-    result += `--- /dev/null\n`;
-    result += `+++ b/${file}\n`;
-    content.split("\n").forEach((line) => {
-      result += `+${line}\n`;
-    });
-  }
-  return result;
-}
-
-export async function getProposedCommits() {
+export async function getProposedCommits(config: LLMConfig) {
   const gitDiff = getGitDiff();
   const newFiles = getUntrackedFiles();
-  console.log("newFiles", newFiles);
-  console.log("diffs", gitDiff);
+
   const newFilesDiff = await generateDiffForNewFiles(newFiles);
 
   const combinedDiff = gitDiff + "\n" + newFilesDiff;
-  const commits = await analyzeGitDiff(combinedDiff);
+  const commits = await analyzeGitDiff(combinedDiff, config);
   return commits;
 }
 
 export async function applyCommits(commits: Commit[]) {
   for (const commit of commits) {
-    const files = commit.files.map((f) => f.split("—")[0].trim());
-
-    gitAddFiles(files);
+    gitAddFiles(commit.files);
 
     const message = `${commit.title}\n\n${commit.description}`;
     createCommit(message);
